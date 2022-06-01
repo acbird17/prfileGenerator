83eaf9f7ec4674d14df348ceb80305417a7aabe8
Alexb <acbird.ab@gmail.com>
2022-05-31 19:44:47 -0600

    most files created

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require("inquirer");
+const engineer = require("./lib/engineer");
+const manager = require("./lib/manager");
+const intern = require("./lib/intern");
+const fs = require("fs");
