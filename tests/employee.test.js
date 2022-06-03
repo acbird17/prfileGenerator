723c924107227737cb82bd144c0aed19c42ec49d
Alexb <acbird.ab@gmail.com>
2022-06-02 20:44:51 -0600

    final example made

---
+++ tests/employee.test.js
@@ -0,0 +1,48 @@
+const Employee = require("../lib/employee");
+
+test("can instantiate Employee instance", () => {
+  const e = new Employee();
+  expect(typeof e).toBe("object");
+});
+
+test("can set name via constructor arguments", () => {
+  const name = "Alice";
+  const e = new Employee(name);
+  expect(e.name).toBe(name);
+});
+
+test("can set id via constructor argument", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.id).toBe(testValue);
+});
+
+test("can set email via constructor argument", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.email).toBe(testValue);
+});
+
+test("can get name via getName()", () => {
+  const testValue = "Alice";
+  const e = new Employee(testValue);
+  expect(e.getName()).toBe(testValue);
+});
+
+test("can get id via getID()", () => {
+  const testValue = 100;
+  const e = new Employee("Foo", testValue);
+  expect(e.getID()).toBe(testValue);
+});
+
+test("can get email via getEmail()", () => {
+  const testValue = "test@test.com";
+  const e = new Employee("Foo", 1, testValue);
+  expect(e.getEmail()).toBe(testValue);
+});
+
+test('getTitle() should return "Employee"', () => {
+  const testValue = "Employee";
+  const e = new Employee("Alice", 1, "test@test.com");
+  expect(e.getTitle()).toBe(testValue);
+});
---
+++ tests/manager.test.js
@@ -0,0 +1,20 @@
+const Manager = require("../lib/manager");
+const Employee = require("../lib/employee");
+
+test("can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.office).toBe(testValue);
+});
+
+test('getTitle() should return "Manager"', () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getTitle()).toBe(testValue);
+});
+
+test("can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOffice()).toBe(testValue);
+});
