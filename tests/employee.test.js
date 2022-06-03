5e6e02ab2297dbe6bb4ad6f45e2b03da52da2f61
Alexb <acbird.ab@gmail.com>
2022-06-03 08:02:10 -0600

    fix variables

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -6,43 +6,43 @@ test("can instantiate Employee instance", () => {
 });
 
 test("can set name via constructor arguments", () => {
-  const name = "Alice";
+  const name = "Alex";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
 
 test("can set id via constructor argument", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Test", testValue);
   expect(e.id).toBe(testValue);
 });
 
 test("can set email via constructor argument", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Test", 1, testValue);
   expect(e.email).toBe(testValue);
 });
 
 test("can get name via getName()", () => {
-  const testValue = "Alice";
+  const testValue = "Alex";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
 
 test("can get id via getID()", () => {
   const testValue = 100;
-  const e = new Employee("Foo", testValue);
+  const e = new Employee("Test", testValue);
   expect(e.getID()).toBe(testValue);
 });
 
 test("can get email via getEmail()", () => {
   const testValue = "test@test.com";
-  const e = new Employee("Foo", 1, testValue);
+  const e = new Employee("Test", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
 
 test('getTitle() should return "Employee"', () => {
   const testValue = "Employee";
-  const e = new Employee("Alice", 1, "test@test.com");
+  const e = new Employee("Alex", 1, "test@test.com");
   expect(e.getTitle()).toBe(testValue);
 });
--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -2,18 +2,18 @@ const Engineer = require("../lib/engineer");
 
 test("can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer("Test", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
 test('getTitle() should return "Engineer"', () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("Test", 1, "test@test.com", "GitHubUser");
   expect(e.getTitle()).toBe(testValue);
 });
 
 test("can get GitHub username via getGithub()", () => {
   const testValue = "GitHubUser";
-  const e = new Engineer("Foo", 1, "test@test.com", testValue);
+  const e = new Engineer("Test", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
